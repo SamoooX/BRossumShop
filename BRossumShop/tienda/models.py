ceddcc89a150c50f62b692039b7d69d32e5ffab8
Potatinho23 <mat.aninir@duocuc.cl>
2023-06-22 01:06:08 -0400

    Modificacion de modelo

--- BRossumShop/tienda/models.py
+++ BRossumShop/tienda/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100, null=False, verbose_name='Nombre')
-    apellido = models.CharField(max_length=100, verbose_name='Apellido')
-    correo = models.CharField(max_length=200, verbose_name='Correo')
-    contraseña = models.CharField(max_length=12, verbose_name='Contraseña')
-    contraseña2 = models.CharField(max_length=12, verbose_name='Contraseña 2')
+    apellido = models.CharField(max_length=100, null= False,verbose_name='Apellido')
+    correo = models.CharField(max_length=200, null= False verbose_name='Correo')
+    contraseña = models.CharField(max_length=12, null= False verbose_name='Contraseña')
+    contraseña2 = models.CharField(max_length=12, null= False verbose_name='Contraseña 2')
 
 
     def str(self):
