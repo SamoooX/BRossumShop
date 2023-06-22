ea3166dbdc705b663dcbfaad37da220831530819
Potatinho23 <mat.aninir@duocuc.cl>
2023-06-22 01:09:47 -0400

    modificacion de modelo 3

--- BRossumShop/tienda/models.py
+++ BRossumShop/tienda/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100, null=False, verbose_name='Nombre')
     apellido = models.CharField(max_length=100, null= False,verbose_name='Apellido')
-    correo = models.CharField(max_length=200, null= False, verbose_name='Correo')
+    correo = models.EmailField(max_length=200, verbose_name='Correo')
     contraseña = models.CharField(max_length=12, null= False, verbose_name='Contraseña')
     contraseña2 = models.CharField(max_length=12, null= False, verbose_name='Contraseña 2')
 
