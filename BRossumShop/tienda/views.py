570c9af361bbb56d63863e44986837be8924a0c7
samuel garrido fuentes <sam.garrido@duocuc.cl>
2023-06-22 17:10:07 -0400

    Agregar dunciones de eliminar carrito y finalizar compra

--- BRossumShop/tienda/views.py
+++ BRossumShop/tienda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
-from .models import Carrito, Producto, DetalleCarrito
+from datetime import datetime
+from .models import Carrito, Producto, DetalleCarrito,  Boleta, DetalleBoleta
 # Create your views here.
 
 def index(request):
@@ -57,3 +58,31 @@ def agregar_carrito(request, id):
 
     # Redireccionar a la página del carrito o a donde desees
     return redirect(to='producto')  # Reemplaza 'carrito' con el nombre de tu URL para la página del carrito
+
+def eliminar_dcarrito(request, id):
+    detalleC = DetalleCarrito.objects.get(id=id)
+    detalleC.delete()
+
+    return redirect(to="carrito")
+
+def finalizar_compra(request):
+    # Obtener el carrito actual del usuario (puedes usar el ID del usuario)
+    carrito = Carrito.objects.get(usuario=request.user)
+
+    # Crear una nueva instancia de Boleta con los datos necesarios
+    boleta = Boleta(usuario=request.user, monto=carrito.total, fecha=datetime.now())
+    boleta.save()
+
+    # Obtener todos los detalles del carrito asociados al carrito actual
+    detalles_carrito = DetalleCarrito.objects.filter(carrito=carrito)
+
+    # Transferir los detalles del carrito a la tabla DetalleBoleta
+    for detalle_carrito in detalles_carrito:
+        detalle_boleta = DetalleBoleta(boleta=boleta, producto=detalle_carrito.producto, valor_unitario=detalle_carrito.producto.precio, cantidad=detalle_carrito.cantidad, total=detalle_carrito.total)
+        detalle_boleta.save()
+
+    # Eliminar los registros del carrito actual en DetalleCarrito
+    detalles_carrito.delete()
+
+    # Redireccionar a una página de confirmación o a otra vista según sea necesario
+    return redirect('confirmacion_compra')
