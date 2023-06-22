5dc379259cf2b87aa65281b60f4b074690b00e2c
Potatinho23 <mat.aninir@duocuc.cl>
2023-06-21 21:02:35 -0400

    Añadí archivo forms

--- BRossumShop/tienda/models.py
+++ BRossumShop/tienda/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100, null=False, verbose_name='Nombre')
+    apellido = models.CharField(max_length=100)
+    correo = models.CharField(max_length=200)
+    contraseña = models.CharField(max_length=12)
